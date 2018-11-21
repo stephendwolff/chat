2ac0151df0a17949e2c9485d88a859f292e3a017
Stephen Wolff <stephen@maxgatedigital.com>
2018-11-21 16:48:56 +0000

    Wait for MySQL before running Django management commands

--- backend/execute.sh
+++ backend/execute.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+
+# nginx
+
+until nc -z -v -w30 db 3306
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
+echo "DB Connected"
+
 python manage.py makemigrations --noinput --settings=$DJANGO_SETTINGS_MODULE
 python manage.py migrate --noinput --settings=$DJANGO_SETTINGS_MODULE
 python manage.py collectstatic --noinput
